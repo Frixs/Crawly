127607d68a03be47a9f9a8c9857a616b3612ea38
Frixs <17912365+Frixs@users.noreply.github.com>
2021-04-27 11:57:28 +0200

    Added UC for setting Data Instance

---
+++ InformationRetrievalManager/InformationRetrievalManager/Controls/CrawlerConfigurationForm.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace InformationRetrievalManager
+{
+    /// <summary>
+    /// Interaction logic for CrawlerConfigurationForm.xaml
+    /// </summary>
+    public partial class CrawlerConfigurationForm : UserControl
+    {
+        #region New Instance Getter (Design)
+
+        /// <summary>
+        /// Create a new instance of this class.
+        /// </summary>
+        public static CrawlerConfigurationForm DesignInstance => new CrawlerConfigurationForm();
+
+        #endregion
+
+        public CrawlerConfigurationForm()
+        {
+            InitializeComponent();
+
+            if (DesignerProperties.GetIsInDesignMode(this))
+            {
+                // Design-mode specific functionality
+            }
+        }
+    }
+}
---
+++ InformationRetrievalManager/InformationRetrievalManager/Controls/ProcessingConfigurationForm.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace InformationRetrievalManager
+{
+    /// <summary>
+    /// Interaction logic for ProcessingConfigurationForm.xaml
+    /// </summary>
+    public partial class ProcessingConfigurationForm : UserControl
+    {
+        #region New Instance Getter (Design)
+
+        /// <summary>
+        /// Create a new instance of this class.
+        /// </summary>
+        public static ProcessingConfigurationForm DesignInstance => new ProcessingConfigurationForm();
+
+        #endregion
+
+        public ProcessingConfigurationForm()
+        {
+            InitializeComponent();
+
+            if (DesignerProperties.GetIsInDesignMode(this))
+            {
+                // Design-mode specific functionality
+            }
+        }
+    }
+}
