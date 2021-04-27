bf424b4a5991f1039c5d6f2609e6af05e5e96856
Frixs <17912365+Frixs@users.noreply.github.com>
2021-04-27 12:52:49 +0200

    Added validation base structures

---
+++ InformationRetrievalManager/InformationRetrievalManager.Core/Attributes/Validations/Base/IValidableAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InformationRetrievalManager.Core
+{
+    /// <summary>
+    /// Base interface for all validation attribute types.
+    /// Works like a marker for <see cref="IValidableAttribute{T}"/> validations.
+    /// </summary>
+    public interface IValidableAttribute
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Formatted property name / label of this validation
+        /// </summary>
+        string ValidationName { get; }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Interface for all validation attribute types.
+    /// All validators under this adds logic to validate specific type of the attributed property.
+    /// </summary>
+    /// <typeparam name="T">The type</typeparam>
+    public interface IValidableAttribute<T> : IValidableAttribute
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Validate value
+        /// </summary>
+        /// <param name="value">The value to validate</param>
+        /// <returns>Validation result instance</returns>
+        DataValidationResult Validate(T value);
+
+        /// <summary>
+        /// Validate value
+        /// </summary>
+        /// <param name="value">The value to validate</param>
+        /// <param name="none">Placeholder to trigger this method - leave null</param>
+        /// <returns>Collection of errors - no erros (empty collection) = value is valid</returns>
+        ICollection<DataValidationError> Validate(T value, object none);
+
+        #endregion
+    }
+}
---
+++ InformationRetrievalManager/InformationRetrievalManager.Core/Validation/DataValidationError.cs
@@ -0,0 +1,18 @@
+﻿namespace InformationRetrievalManager.Core
+{
+    /// <summary>
+    /// Encapsulates an error from the <see cref="DataValidationResult"/>.
+    /// </summary>
+    public class DataValidationError
+    {
+        /// <summary>
+        /// Gets or sets the code for this error.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description for this error.
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
