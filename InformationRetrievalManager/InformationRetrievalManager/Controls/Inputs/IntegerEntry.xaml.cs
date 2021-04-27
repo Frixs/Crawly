4f19337fdb90e7e5d887e6961f6eb8a18a4c2fd4
Frixs <17912365+Frixs@users.noreply.github.com>
2021-04-27 16:43:58 +0200

    Integer entry

---
+++ InformationRetrievalManager/InformationRetrievalManager/Controls/Inputs/IntegerEntry.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace InformationRetrievalManager
+{
+    /// <summary>
+    /// Interaction logic for IntegerEntry.xaml
+    /// </summary>
+    public partial class IntegerEntry : UserControl
+    {
+        public IntegerEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ InformationRetrievalManager/InformationRetrievalManager/ViewModels/Inputs/Design/IntegerEntryDesignModel.cs
@@ -0,0 +1,26 @@
+﻿namespace InformationRetrievalManager
+{
+    public class IntegerEntryDesignModel : IntegerEntryViewModel
+    {
+        #region New Instance Getter (Design)
+
+        /// <summary>
+        /// Create a new instance of this class.
+        /// </summary>
+        public static IntegerEntryDesignModel Instance => new IntegerEntryDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public IntegerEntryDesignModel()
+        {
+
+        }
+
+        #endregion
+    }
+}
