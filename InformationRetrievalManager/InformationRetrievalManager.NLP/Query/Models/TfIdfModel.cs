c271b1079631a7bb62390958906895032a9221be
Frixs <17912365+Frixs@users.noreply.github.com>
2021-05-06 22:12:30 +0200

    TF-IDF vector calculation optimalization

--- InformationRetrievalManager/InformationRetrievalManager.NLP/Query/Models/TfIdfModel.cs
+++ InformationRetrievalManager/InformationRetrievalManager.NLP/Query/Models/TfIdfModel.cs
@@ -29,12 +29,12 @@ namespace InformationRetrievalManager.NLP
         /// <summary>
         /// Calculated document vectors based on the last run of <see cref="CalculateData"/>.
         /// </summary>
-        private Dictionary<long, double[]> _documentVectors = null;
+        private Dictionary<long, (uint Position, double Value)[]> _documentVectors = null;
 
         /// <summary>
         /// Calculated query vector based on the last run of <see cref="CalculateQuery"/>.
         /// </summary>
-        private double[] _queryVector = null;
+        private (uint Position, double Value)[] _queryVector = null;
 
         #endregion
 
@@ -97,7 +97,7 @@ namespace InformationRetrievalManager.NLP
             totalDocuments = documents.Count;
 
             // Create array for document vectors
-            var docVectors = new Dictionary<long, double[]>();
+            var docVectors = new Dictionary<long, (uint Position, double Value)[]>();
 
             // Go through documents and create document vectors...
             i = 0;
@@ -172,7 +172,7 @@ namespace InformationRetrievalManager.NLP
                 throw new InvalidOperationException("Query vector is not defined!");
 
             // Calculate cosine similarity for each document...
-            var results = new List<(long, double)>();
+            var results = new List<(long DocumentId, double Relevance)>();
             long i = 0;
             foreach (var document in documentVectors)
             {
@@ -189,12 +189,12 @@ namespace InformationRetrievalManager.NLP
 
             // Sort and return result
             setProgressMessage?.Invoke("retrieving results");
-            //results.Sort((x, y) => y.Item2.CompareTo(x.Item2));
-            results = results.OrderByDescending(o => o.Item2).ThenBy(o => o.Item1).ToList();
+            //results.Sort((x, y) => y.Relevance.CompareTo(x.Relevance));
+            results = results.OrderByDescending(o => o.Relevance).ThenBy(o => o.DocumentId).ToList();
             foundDocuments = results.Count;
             if (select > 0)
-                return results.Select(o => o.Item1).Take(select).ToArray();
-            return results.Select(o => o.Item1).ToArray();
+                return results.Select(o => o.DocumentId).Take(select).ToArray();
+            return results.Select(o => o.DocumentId).ToArray();
         }
 
         #endregion
@@ -206,27 +206,55 @@ namespace InformationRetrievalManager.NLP
         /// </summary>
         /// <param name="queryVector">Query vector</param>
         /// <param name="documentVector">Document vector</param>
-        /// <returns>Cosine similarity result</returns>
+        /// <returns>Cosine similarity result of document relevance.</returns>
         /// <exception cref="ArgumentNullException">Missing query or document vector reference.</exception>
-        private double CalculateCosSimilarity(double[] queryVector, double[] documentVector)
+        /// <remarks>
+        ///     The vectors are created by <see cref="CalculateDocumentVector"/> and this vector calculation is based on the process of creation.
+        /// </remarks>
+        private double CalculateCosSimilarity((uint Position, double Value)[] queryVector, (uint Position, double Value)[] documentVector)
         {
             if (queryVector == null || documentVector == null)
                 throw new ArgumentNullException("Vectors not specified!");
 
+            // Query SUM calculation
             double queryPowSum = 0;
-            foreach (var value in queryVector)
-                queryPowSum += value * value;
+            foreach (var (Index, Value) in queryVector)
+                queryPowSum += Value * Value;
             double querySum = Math.Sqrt(queryPowSum);
 
+            // Document SUM calculation
             double documentPowSum = 0;
-            foreach (var value in documentVector)
-                documentPowSum += value * value;
+            foreach (var (Index, Value) in documentVector)
+                documentPowSum += Value * Value;
             double documentSum = Math.Sqrt(documentPowSum);
 
+            // X calculation
             double x = 0;
-            for (int i = 0; i < queryVector.Length; i++)
-                x += queryVector[i] * documentVector[i];
+            int qIdx = 0; // array index of query vector values
+            int dIdx = 0; // array index of document vector values
+            while (true)
+            {
+                // If one of the vectors already reached its end...
+                if (qIdx >= queryVector.Length || dIdx >= documentVector.Length)
+                    // ...break the calculation, no need for further calculation due to zero multiplication
+                    break;
 
+                // Get the current positions
+                var qPos = queryVector[qIdx].Position;
+                var dPos = documentVector[dIdx].Position;
+
+                // Calculate only if vector positions match
+                if (qPos == dPos)
+                    x += queryVector[qIdx].Value * documentVector[dIdx].Value;
+
+                // Balance the vector positions
+                if (qPos > dPos)
+                    dIdx++;
+                else
+                    qIdx++;
+            }
+
+            // Make the final calculation
             return x / (documentSum * querySum);
         }
 
@@ -236,10 +264,19 @@ namespace InformationRetrievalManager.NLP
         /// <param name="data">The data</param>
         /// <param name="documentId">The ID</param>
         /// <param name="cancellationToken">Cancellation token for interrupting the process.</param>
-        /// <returns>Document vector from <paramref name="data"/> based on <paramref name="documentId"/>.</returns>
-        private double[] CalculateDocumentVector(IReadOnlyDictionary<string, IReadOnlyDictionary<long, IReadOnlyTermInfo>> data, long documentId, CancellationToken cancellationToken = default)
+        /// <returns>
+        ///     Document vector made from <paramref name="data"/> based on <paramref name="documentId"/>
+        ///     represented with tuple where (position indexes are in ASC order): 
+        ///         1=(term position index), 
+        ///         2=(vector position value)
+        /// </returns>
+        /// <remarks>
+        ///     Vector stores only non-zero values to optimize the TF-IDF calculation process.
+        /// </remarks>
+        private (uint Position, double Value)[] CalculateDocumentVector(IReadOnlyDictionary<string, IReadOnlyDictionary<long, IReadOnlyTermInfo>> data, long documentId, CancellationToken cancellationToken = default)
         {
-            List<double> docVector = new List<double>();
+            var docVector = new List<(uint Position, double Value)>();
+            uint i = 0;
             foreach (var term in data)
             {
                 // Check for cancelation
@@ -253,13 +290,11 @@ namespace InformationRetrievalManager.NLP
                     double tf = term.Value[documentId].Frequency; // Term Frequency
                     double ntf = tf > 0 ? 1 + Math.Log(tf, 10) : 0; // 1 + log(TF) --- if TF == 0 => 0
                     double tfidf = _termIdf.ContainsKey(term.Key) ? ntf * _termIdf[term.Key] : 0; // If the term does not exist (query call), TF-IDF of the term is 0
-                    docVector.Add(tfidf);
+                    docVector.Add((i, tfidf));
                 }
                 // Otherwise, it is 0...
-                else
-                {
-                    docVector.Add(0);
-                }
+
+                i++;
             }
 
             return docVector.ToArray();
