3532f1be7d264bdd935a5bfeb2733c2459965bad
Frixs <17912365+Frixs@users.noreply.github.com>
2021-05-08 20:57:14 +0200

    additional push for previous commit

---
+++ InformationRetrievalManager/InformationRetrievalManager.Relational/Repositories/IIndexedDocumentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InformationRetrievalManager.Relational
+{
+    /// <summary>
+    /// Public repository interface for <see cref="IndexedDocumentRepository"/>
+    /// </summary>
+    public interface IIndexedDocumentRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        /// <summary>
+        /// Gets the highest (latest) document timestamp.
+        /// </summary>
+        /// <returns>Latest indexed document timestamp.</returns>
+        DateTime GetMaxTimestamp();
+    }
+}
